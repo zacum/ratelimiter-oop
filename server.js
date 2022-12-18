926f7a2e5021a1e453beb9586361fbf3daca2792
zacum <mfilgyhty@hotmail.com>
2022-12-18 16:02:03 +0900

    dev: integrate rateLimiter with server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 import express from "express";
+import { RateLimiterMiddleware } from "./src/middleware/rateLimiterMiddleware.js";
 
 const app = express();
 
+const rateLimiterMiddleware = new RateLimiterMiddleware({
+    TIME_FRAME_IN_S: 10,
+    RPS_LIMIT: 2
+})
+app.use(rateLimiterMiddleware.rateLimiter);
 
 app.get('/', (req, res) => {
     res.send('I am here.')
